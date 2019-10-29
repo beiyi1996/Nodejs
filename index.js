e1ff5e7913524274f5c7b47323ad6b60d73b8d4e
Winni Huang <usj0326@gmail.com>
2019-10-29 16:38:39 +0800

    add index.js and memver schema

---
+++ index.js
@@ -0,0 +1,45 @@
+/*jshint esversion: 6 */
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const cors = require('cors');
+// const router = require('./router');
+const port = 3000;
+
+app.set('views', path.join(__dirname, './views/')); // 默認就是views目錄
+
+app.use(cors());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+app.use(
+  session({
+    secret: "youarehash", // 加密字串: 會與原密碼組合成新的字串, 在進行加密
+    resave: false,
+    saveUninitialized: true // session默認直接分配給你一個加密過的key,
+    // false: server 真的存資料到 session時, 才分配cookie key
+  })
+);
+
+// app.use(router);
+
+app.get('/', (req, res) => {
+    res.send('hi!!!');
+});
+
+app.use((req, res) => {
+    res.send('404 Not found...');
+});
+
+app.use((err, req, res, next) => {
+    res.status(500).json({
+        err_code: 500,
+        message: err.message
+    });
+});
+
+app.listen(port, () => console.log('server is running at port 3000...'));
---
+++ models/member.js
@@ -0,0 +1,41 @@
+/*jshint esversion: 6 */
+
+const mongoose = require("mongoose");
+let Schema = mongoose.Schema;
+
+mongoose.connect("mongodb://localhost/booking", { useMongoClient: true });
+
+let memberSchema = new Schema({
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String
+  },
+  phone: {
+    type: String,
+    validate: {
+      validator: function(v) {
+        return /\d{4}-\d{3}-\d{3}/.test(v);
+      }
+    }
+  },
+  gender: {
+    type: Number,
+    enum: [0, 1],
+    default: 0
+  },
+  create_time: {
+    type: Date,
+    default: Date.now
+  },
+  modified_time: {
+    type: Date,
+    default: Date.now
+  }
+});
