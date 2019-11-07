eb335947a253746fd787e4fd48473cf9c0ac87b4
Winni Huang <usj0326@gmail.com>
2019-11-07 18:05:11 +0800

    add restaurant router

--- src/router.js
+++ src/router.js
@@ -3,7 +3,8 @@
 const express = require("express");
 const path = require("path");
 const router = express.Router();
-const MemberController = require("./controllers/member");
+const MemberController = require("./controllers/memberControllers");
+const RestaurantController = require("./controllers/restaurantControllers");
 
 // 會員註冊
 router.get("/register", (req, res) => {
@@ -26,5 +27,14 @@ router.get("/forgotpassword", MemberController.forgotPassword);
 router.get("/modifiedpassword", MemberController.modifiedPasswordGET);
 
 router.post("/modifiedpassword", MemberController.modifiedPasswordPOST);
+
+// 加入餐廳資料
+router.post("/insertrestaurantdata", RestaurantController.createRestaurantData);
+
+// 更新餐廳資料
+router.post("/updaterestaurantdata", RestaurantController.updateRestaurantData);
+
+// 刪除資料
+router.post("/deleterestaurantdata", RestaurantController.deleteRestaurantData);
 
 module.exports = router;
