90614febc607dc1ed91b9a1b87b9ad73f228d137
Winni Huang <usj0326@gmail.com>
2019-11-08 18:00:04 +0800

    add random render restaurant api

---
+++ src/controllers/indexControllers.js
@@ -0,0 +1,15 @@
+/*jshint esversion: 6 */
+
+import Restaurant from "../../models/restaurant";
+
+exports.randomRenderRestaurant = (req, res, next) => {
+    Restaurant.find((err, data) => {
+        if(err) next(err);
+        console.log('data', data);
+        res.status(200).json({
+            code: 200,
+            message: "OK",
+            randomRestaurants: data
+        });
+    }).limit(10);
+};
