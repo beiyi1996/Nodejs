27f7faf468bfbb26f510eb24ba9dc2727d540b07
Winni Huang <usj0326@gmail.com>
2019-11-21 18:12:56 +0800

    add createOrder feature

--- src/controllers/orderControllers.js
+++ src/controllers/orderControllers.js
@@ -1,10 +1,31 @@
 /*jshint esversion: 6 */
 
 import Order from "../../models/order";
+import Member from "../../models/member";
 
-const createOrder = (req, res, next) => {
+const createOrder = async (req, res, next) => {
     const { date, time, adult, children, nots, restaurant_id } = req.body;
-    console.log('req.session', req.session);
+    let member_Id = "";
+    console.log('-------req.session', req.session.member);
+    if(!req.session.isLogIn) {
+        res.redirect('/login');
+    }
+    Member.findOne({name: req.session.member}, (err, member) => {
+        if(err) next(err);
+        member_Id = member_id;
+    });
+    const order = await Order.create({
+        date,
+        time,
+        adult,
+        children,
+        nots,
+        restaurant_id
+    });
+    res.status(200).json({
+        code: 200,
+        message: 'OK'
+    });
 };
 
 module.exports = { createOrder };
