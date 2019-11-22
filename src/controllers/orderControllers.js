6ee3cee5e3ff1c32ff1992cc15b918e795998f85
Winni Huang <usj0326@gmail.com>
2019-11-22 18:18:13 +0800

    fix get member_id and restaurant_id method

--- src/controllers/orderControllers.js
+++ src/controllers/orderControllers.js
@@ -2,30 +2,44 @@
 
 import Order from "../../models/order";
 import Member from "../../models/member";
+import Restaurant from "../../models/restaurant";
 
 const createOrder = async (req, res, next) => {
-    const { date, time, adult, children, nots, restaurant_id } = req.body;
-    let member_Id = "";
-    console.log('-------req.session', req.session.member);
-    if(!req.session.isLogIn) {
-        res.redirect('/login');
+    try{
+        const { date, time, adult, children, nots, restaurant_name } = req.body;
+        console.log('-------req.session', req.session.member);
+        if(!req.session.isLogIn) {
+            console.log('no log in');
+            return res.redirect('/login');
+        }
+        const member_id = await Member.findOne({name: req.session.member}, (err, member) => {
+            if(err) next(err);
+            console.log('member', member._id);
+            return member._id;
+        });
+        console.log('member_id', member_id);
+        console.log('restaurant_name', restaurant_name);
+        const restaurant_id = await Restaurant.findOne({name: restaurant_name}, (err, restaurant) => {
+            if(err) next(err);
+            console.log('restaurant', restaurant._id);
+            return restaurant._id;
+        });
+        console.log('restaurant_id', restaurant_id);
+        const order = await Order.create({
+            date,
+            time,
+            adult,
+            children,
+            nots,
+            restaurant_id,
+            member_id
+        });
+        console.log('order', order);
+        res.json(order);
     }
-    Member.findOne({name: req.session.member}, (err, member) => {
-        if(err) next(err);
-        member_Id = member_id;
-    });
-    const order = await Order.create({
-        date,
-        time,
-        adult,
-        children,
-        nots,
-        restaurant_id
-    });
-    res.status(200).json({
-        code: 200,
-        message: 'OK'
-    });
+    catch (err){
+        return next(err);
+    }
 };
 
 module.exports = { createOrder };
