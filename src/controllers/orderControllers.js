889d957df3f8bf0a25b7a08e169535be515dc0bf
Winni Huang <usj0326@gmail.com>
2019-11-25 00:48:42 +0800

    add sendCompletedMail function

--- src/controllers/orderControllers.js
+++ src/controllers/orderControllers.js
@@ -3,6 +3,30 @@
 import Order from "../../models/order";
 import Member from "../../models/member";
 import Restaurant from "../../models/restaurant";
+import MyEmail from "../mailPassword";
+import NodeMailer from "nodemailer";
+
+async function sendCompletedMail(email, orderId) {
+    console.log(1, MyEmail);
+    console.log(2, orderId);
+    let transporter = NodeMailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: MyEmail.email, // generated ethereal user
+        pass: MyEmail.password // generated ethereal password
+      }
+    });
+  
+    await transporter.sendMail({
+      from: MyEmail.email, // sender address
+      to: email, // list of receivers
+      subject: `Completed Mail ${orderId}`, // Subject line
+      text: "This is your completed email", // plain text body
+      html: `This is your completed email, your order id is ${orderId}. <br> you can checked your order here <a href="/checkorders">check your order</a>` // html body
+    });
+  }
 
 const createOrder = async (req, res, next) => {
     try{
@@ -13,18 +37,19 @@ const createOrder = async (req, res, next) => {
             console.log('no log in');
             return res.redirect('/login');
         }
-        const memberID = await Member.findOne({name: req.session.member}, {_id: 1});
+        const member = await Member.findOne({name: req.session.member}, {_id: 1, email: 1});
         const restaurantID = await Restaurant.findOne({name: restaurant_name},{_id: 1});
-        if(memberID && restaurantID) {
+        if(member && restaurantID) {
             const order = await Order.create({
                 dateTime,
                 adult,
                 children,
                 nots,
                 restaurant_id: restaurantID._id,
-                member_id: memberID._id
+                member_id: member._id
             });
-            res.json(order);
+            await sendCompletedMail(member.email, restaurantID._id);
+            res.json(order).redirect('/complated');
         }else {
             errObj(res,'500','memberID or restaurantID is null');
         }
