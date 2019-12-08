934a87ad1793ec006929af6d8bdba8c3352a5a0e
Winni Huang <usj0326@gmail.com>
2019-12-08 22:57:44 +0800

    remove createCustomerOrder and confirmedCustomerOrder api, use prettier plugin format

--- src/controllers/restaurantControllers.js
+++ src/controllers/restaurantControllers.js
@@ -7,161 +7,165 @@ const createRestaurantData = (req, res, next) => {
   const { name, address } = req.body;
   const body = req.body;
   console.log("restaurant data", req.body);
-  Restaurant.findOne({ name: name, address: address }, (err, data) => {
-    if (err) next(err);
-    if (data) {
-      console.log(222, 'if');
-      return res.status(201).json({
-        code: 201,
-        message: "Restaurant is already exists."
+  Restaurant.findOne(
+    {
+      name: name,
+      address: address
+    },
+    (err, data) => {
+      if (err) next(err);
+      if (data) {
+        console.log(222, "if");
+        return res.status(201).json({
+          code: 201,
+          message: "Restaurant is already exists."
+        });
+      }
+
+      new Restaurant(body).save((err, restaurant) => {
+        if (err) next(err);
+        console.log("restaurant", restaurant);
+        res.status(200).json({
+          code: 200,
+          message: `Create restaurant: ${name}`
+        });
+        console.log("end save..");
       });
     }
-
-    new Restaurant(body).save((err, restaurant) => {
-      if (err) next(err);
-      console.log("restaurant", restaurant);
-      res.status(200).json({
-        code: 200,
-        message: `Create restaurant: ${name}`
-      });
-      console.log('end save..');
-    });
-  });
+  );
 };
 
 const updateRestaurantData = (req, res, next) => {
   const { name } = req.body;
-  console.log('req.body', req.body);
-  Restaurant.findOneAndUpdate({name: name}, req.body, (err, restaurant) => {
-    if(err) next(err);
-    
-    // findOneAndUpdate 回傳的是 未修改前 的資料
-    console.log('update restaurant', restaurant);
-    res.status(200).json({
-      code: 200,
-      message: `Update restaurant: ${name}`
-    });
-    console.log('end...');
-  });
+  console.log("req.body", req.body);
+  Restaurant.findOneAndUpdate(
+    {
+      name: name
+    },
+    req.body,
+    (err, restaurant) => {
+      if (err) next(err);
+
+      // findOneAndUpdate 回傳的是 未修改前 的資料
+      console.log("update restaurant", restaurant);
+      res.status(200).json({
+        code: 200,
+        message: `Update restaurant: ${name}`
+      });
+      console.log("end...");
+    }
+  );
 };
 
 const deleteRestaurantData = (req, res, next) => {
   const { name, address } = req.body;
-  Restaurant.findOneAndDelete({name: name, address: address}, (err, restaurant) => {
-    if(err) next(err);
+  Restaurant.findOneAndDelete(
+    {
+      name: name,
+      address: address
+    },
+    (err, restaurant) => {
+      if (err) next(err);
 
-    console.log('delete restaurant', restaurant);
-    res.status(200).json({
-      code: 200,
-      message: `Delete restaurant: ${name}` 
-    });
-    console.log('delete end..');
-  });
+      console.log("delete restaurant", restaurant);
+      res.status(200).json({
+        code: 200,
+        message: `Delete restaurant: ${name}`
+      });
+      console.log("delete end..");
+    }
+  );
 };
 
-async function findByName(name, res, next){
-  await Restaurant.findOne({name: name}, (err, data) => {
-    if(err) next(err);
-    console.log('data', data);
-    res.status(200).json({
-      code: 200,
-      message: `find restaurant by name : ${name}`,
-      resaturants: data
-    });
-  });
+async function findByName(name, res, next) {
+  await Restaurant.findOne(
+    {
+      name: name
+    },
+    (err, data) => {
+      if (err) next(err);
+      console.log("data", data);
+      res.status(200).json({
+        code: 200,
+        message: `find restaurant by name : ${name}`,
+        resaturants: data
+      });
+    }
+  );
 }
 
-async function findByArea(area, res, next){
-  await Restaurant.find({"category.area": area}, (err, data) => {
-    if(err) next(err);
-    console.log('data', data);
-    res.status(200).json({
-      code: 200,
-      message: `find restaurant by area : ${area}`,
-      restaurants: data
-    });
-  });
+async function findByArea(area, res, next) {
+  await Restaurant.find(
+    {
+      "category.area": area
+    },
+    (err, data) => {
+      if (err) next(err);
+      console.log("data", data);
+      res.status(200).json({
+        code: 200,
+        message: `find restaurant by area : ${area}`,
+        restaurants: data
+      });
+    }
+  );
 }
 
-async function findByKind(kind, res, next){
-  await Restaurant.find({"category.kind": kind}, (err, data) => {
-    if(err) next(err);
-    console.log('data', data);
-    res.status(200).json({
-      code: 200,
-      message: `find restaurant by kind : ${kind}`,
-      restaurants: data
-    });
-  });
+async function findByKind(kind, res, next) {
+  await Restaurant.find(
+    {
+      "category.kind": kind
+    },
+    (err, data) => {
+      if (err) next(err);
+      console.log("data", data);
+      res.status(200).json({
+        code: 200,
+        message: `find restaurant by kind : ${kind}`,
+        restaurants: data
+      });
+    }
+  );
 }
 
 const searchRestaurant = (req, res, next) => {
   const { area, kind, name } = req.query;
-  if(area){
-    console.log('area', area);
+  if (area) {
+    console.log("area", area);
     findByArea(area, res, next);
-  } else if(kind){
-    console.log('kind', kind);
+  } else if (kind) {
+    console.log("kind", kind);
     findByKind(kind, res, next);
-  } else if(name){
-    console.log('name', name);
+  } else if (name) {
+    console.log("name", name);
     findByName(name, res, next);
   }
 };
 
 const getRestaurantDetail = (req, res, next) => {
   const { name, _id } = req.params;
-  console.log('name', name, '_id', _id);
-  Restaurant.findOne({name: name, _id: _id}, (err, data) => {
-    if(err) next(err);
-    console.log('data', data);
-    res.status(200).json({
-      code: 200,
-      message: `find restaurant ${name}`,
-      restaurant: data
-    });
-  });
+  console.log("name", name, "_id", _id);
+  Restaurant.findOne(
+    {
+      name: name,
+      _id: _id
+    },
+    (err, data) => {
+      if (err) next(err);
+      console.log("data", data);
+      res.status(200).json({
+        code: 200,
+        message: `find restaurant ${name}`,
+        restaurant: data
+      });
+    }
+  );
 };
 
-const createCustomerOrder = async(order) => {
-  console.log('order', order);
-  const memberData = await Member.findOne({_id: order.member_id}, {_id: 1, email: 1, name: 1, phone: 1});
-  console.log('memberData', memberData);
-  const newOrder = await SupplierOrders.create({
-    useremail: memberData.email,
-    username: memberData.name,
-    userphone: memberData.phone,
-    adult: order.adult,
-    children: order.children,
-    dateTime: order.dateTime,
-    notes: order.notes
-  });
-  console.log('newOrder', newOrder);
-};
-
-const confirmedCustomerOrder = (req, res, next) => {
-  const { orderStatus, useremail, order_id } = req.query;
-  if(orderStatus === 'OK'){
-    res.status(200).json({
-      code: 200,
-      message: 'Restaurant is confirmed this order',
-      data: {orderStatus, useremail, order_id} 
-    });
-  } else {
-    res.status(403).json({
-      code: 403,
-      message: 'Today is Fully Booked',
-      data: {orderStatus, useremail, order_id}
-    });
-  }
-}
-
-module.exports = { 
-  createRestaurantData, 
-  updateRestaurantData, 
-  deleteRestaurantData, 
-  searchRestaurant, 
-  getRestaurantDetail, 
-  createCustomerOrder, 
-  confirmedCustomerOrder 
+module.exports = {
+  createRestaurantData,
+  updateRestaurantData,
+  deleteRestaurantData,
+  searchRestaurant,
+  getRestaurantDetail
 };
