72eef944824747a46f6097b5ebe03bd36e8fe7f8
Winni Huang <usj0326@gmail.com>
2019-12-16 18:07:36 +0800

    import forgotPassword, search, detail, booking  page

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,16 @@ import Completed from "./views/Completed";
 import LogIn from "./views/LogIn";
 import Register from "./views/Register";
 import ModifiedPassword from "./views/ModifiedPassword";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ForgotPassword from "./views/ForgotPassword";
+import Search from "./views/Search";
+import Detail from "./views/Detail";
+import Booking from "./views/Booking";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <ModifiedPassword />
+      <Booking />
     </div>
   );
 }
