cd0b54f4145b57a1e98dccb24ba5072301a7db99
Winni Huang <usj0326@gmail.com>
2020-01-16 18:11:35 +0800

    modified password input and forgotpassword button

--- client/src/views/LogIn.js
+++ client/src/views/LogIn.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import { useHistory } from "react-router-dom";
+import productService from "../services/productService";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
+import FormControl from "@material-ui/core/FormControl";
+import clsx from "clsx";
+import InputLabel from "@material-ui/core/InputLabel";
+import Input from "@material-ui/core/Input";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import IconButton from "@material-ui/core/IconButton";
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -39,14 +49,52 @@ const useStyles = makeStyles(theme => ({
     justifyContent: "center"
   },
   button: {
-    width: "40%",
-    margin: "50px 0",
+    margin: "10px 0",
     fontFamily: "Microsoft JhengHei"
   }
 }));
 
 function LogIn() {
   const classes = useStyles();
+  const [email, setEmail] = useState("");
+  const [values, setValues] = useState({
+    amount: "",
+    password: "",
+    weight: "",
+    weightRange: "",
+    showPassword: false
+  });
+  const history = useHistory();
+
+  const handleChange = prop => event => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword });
+  };
+
+  const handleMouseDownPassword = event => {
+    event.preventDefault();
+  };
+
+  const handleEmailChange = e => {
+    setEmail(e.target.value);
+  };
+
+  const handleLogIn = async (email, password) => {
+    const logIn = await productService.logIn(email, password);
+    console.log("logIn", logIn);
+  };
+
+  const handleForgetPassword = () => {
+    console.log("handleforgetpassword is working");
+    if (email === "") {
+      alert("請先填入註冊時的email");
+    } else {
+      history.push("/forgotpassword");
+    }
+  };
 
   return (
     <Container maxWidth="sm">
@@ -56,13 +104,36 @@ function LogIn() {
         </Grid>
         <Grid item xs={12} className={classes.formGrid}>
           <form className={classes.form} noValidate autoComplete="off">
-            <TextField label="email" name="email" className={classes.input} />
-            <TextField label="password" name="password" className={classes.input} />
+            <TextField label="email" name="email" className={classes.input} onChange={handleEmailChange} />
+            <FormControl className={clsx(classes.margin, classes.textField, classes.input)}>
+              <InputLabel htmlFor="standard-adornment-password">password</InputLabel>
+              <Input
+                id="standard-adornment-password"
+                type={values.showPassword ? "text" : "password"}
+                value={values.password}
+                onChange={handleChange("password")}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                    >
+                      {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                name="password"
+              />
+            </FormControl>
           </form>
         </Grid>
         <Grid item xs={12} className={classes.buttonGrid}>
-          <Button variant="outlined" className={classes.button}>
+          <Button variant="outlined" className={classes.button} onClick={() => handleLogIn(email, values.password)}>
             登入
+          </Button>
+          <Button variant="outlined" className={classes.button} onClick={handleForgetPassword}>
+            忘記密碼
           </Button>
         </Grid>
       </Grid>
