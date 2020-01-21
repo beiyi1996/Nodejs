3cd8d8f0faae013cd4dacd25549706f7a5191da4
Winni Huang <usj0326@gmail.com>
2020-01-21 18:03:32 +0800

    modified session setting and change cros execute position

--- index.js
+++ index.js
@@ -14,14 +14,17 @@ const MongoStore = connectMongo(session);
 app.set("views", path.join(__dirname, "./views/")); // 默認就是views目錄
 app.use("/node_modules/", express.static(path.join(__dirname, "./node_modules/")));
 
-app.use(cors());
 app.use(cookieParser("38940293"));
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
 app.use(
   session({
     secret: "youarehash", // 加密字串: 會與原密碼組合成新的字串, 在進行加密
-    resave: false,
+    resave: true,
     saveUninitialized: true, // session默認直接分配給你一個加密過的key,false: server 真的存資料到 session時, 才分配cookie key
     store: new MongoStore({ url: "mongodb://localhost:27017/sessiondb" }),
     cookie: {
@@ -31,9 +34,8 @@ app.use(
     }
   })
 );
+app.use(cors());
 
-// parse application/json
-app.use(bodyParser.json());
 app.use(router);
 
 app.get("*", (req, res) => {
