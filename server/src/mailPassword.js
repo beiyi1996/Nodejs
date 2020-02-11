0250f25df222715059a3590c31ebad51fadbcfa0
Winni Huang <usj0326@gmail.com>
2020-02-12 01:14:39 +0800

    modified folder architecture

--- server/src/mailPassword.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  email: "gourmand369@gmail.com",
-  password: "ao3g6ru8",
-  googleMapKey: "AIzaSyA0vT9ooDqSq14SjW9ST3SeOMOfbNwRLJk",
-  mongo: {
-    connection:
-      "mongodb+srv://winni:c0955822647@winnimongodb-ico3v.azure.mongodb.net/booking?retryWrites=true&w=majority"
-  }
-};
