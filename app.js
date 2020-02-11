addadca5ed866dc19fc58cc0c0d0d387dffd333f
Winni Huang <usj0326@gmail.com>
2020-02-12 02:00:54 +0800

    move out app.js

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ import path from "path";
 import bodyParser from "body-parser";
 import session from "express-session";
 import cookieParser from "cookie-parser";
-import router from "./router";
+import routes from "./router";
 import cors from "cors";
 import connectMongo from "connect-mongo";
 import mongoose from "mongoose";
 import mailPassword from "./mailPassword";
-import { CloudFormation } from "aws-sdk";
 const app = express();
 const port = process.env.PORT || 5000;
 const MongoStore = connectMongo(session);
@@ -62,7 +61,7 @@ app.use(
 );
 app.use(cors());
 
-app.use(router);
+app.use("/", routes);
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
