078a5ddfdc42838c33b11677603cac0f784cc224
unknown <winniwhuang@liontravel.com>
2020-02-14 11:43:52 +0800

    add oauth and account property

--- src/mailPassword.js
+++ src/mailPassword.js
@@ -1,9 +1,20 @@
 module.exports = {
-  email: "gourmand369@gmail.com",
-  password: "ao3g6ru8",
+  account: {
+    email: "gourmand369@gmail.com",
+    password: "ao3g6ru8"
+  },
   googleMapKey: "AIzaSyA0vT9ooDqSq14SjW9ST3SeOMOfbNwRLJk",
   mongo: {
     connection:
       "mongodb+srv://winni:c0955822647@winnimongodb-ico3v.azure.mongodb.net/booking?retryWrites=true&w=majority"
+  },
+  oauth: {
+    clientId: "1007038740753-t0m760q08n0doh46ng95hmr554acflhp.apps.googleusercontent.com",
+    clientSecret: "q-qXihtu45d1p5G-Qzyjw3vv",
+    refreshToken:
+      "1//04cOysCc1-zinCgYIARAAGAQSNwF-L9IrfsnKzNkQfd2k8jFf76CP-OYA75L7blxCvpU2srweQJQb5w_dxzjm2wRrAigwf5ibCdo",
+    accessToken:
+      "ya29.Il-9B8zw49hULghI8ra1Xp0tbbemRlJ1lx4L89rXg0Fm5552meeFFGMUOPY_vi9GXfHd6_i0M7DuyAQs51J_d_RCh8-VBKU_kJ-JltQeB9GNOqeXwZVOm6IPhJAjlXN0Tg",
+    expries: 1609344000000
   }
 };
